b8156170edf86911125e9957097efc3d23d6cdd8
Leigh Allen <lallenation@gmail.com>
2024-02-22 15:03:21 +0000

    add basic styling to css, link css to project

--- Arcade/settings.py
+++ Arcade/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-leighallendev-bttav2-nl1ignjnd86.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['.gitpod.io',]
 
 
 # Application definition
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
