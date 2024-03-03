b111bc3db5e39af3339c6ee97c8ec48d4d580ed3
Leigh Allen <lallenation@gmail.com>
2024-03-03 21:39:50 +0000

    add: token sections added to bundles model

--- bundles/models.py
+++ bundles/models.py
@@ -7,6 +7,8 @@ class Bundle(models.Model):
     # Assuming a Category model that includes both food and drink categories
     food_categories = models.ManyToManyField(Category, related_name='food_bundle', limit_choices_to={'type': 'food'})
     drink_categories = models.ManyToManyField(Category, related_name='drink_bundle', limit_choices_to={'type': 'drink'})
+    food_tokens = models.IntegerField(default=0)
+    drink_tokens = models.IntegerField(default=0)
     number_of_people = models.IntegerField(default=1)
     playtime_hours = models.DecimalField(max_digits=4, decimal_places=2, default=0.0)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
