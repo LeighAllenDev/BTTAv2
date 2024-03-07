81c3099eda3caec75df34e3061bf4fc5c5deb2d9
Leigh Allen <lallenation@gmail.com>
2024-03-07 12:54:37 +0000

    add: api endpoint for bundles view

--- bundles/views.py
+++ bundles/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from .models import Bundle
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import BundleSerializer
 
 # Create your views here.
 def bundles(request):
     bundles = Bundle.objects.prefetch_related('categories').all()
     template_name = "bundles/bundles.html"
     return render(request, template_name, {'bundles': bundles})
+
+
+@api_view(['GET'])
+def bundle_detail(request, pk):
+    try:
+        bundle = Bundle.objects.get(pk=pk)
+        serializer = BundleSerializer(bundle)
+        return Response(serializer.data)
+    except Bundle.DoesNotExist:
+        return Response({'error': 'Bundle not found'}, status=404)
