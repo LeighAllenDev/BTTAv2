35c3c4e3db64dee9e2c10ca9865513646e5eb94c
Leigh Allen <lallenation@gmail.com>
2024-03-26 23:19:13 +0000

    added styling to accounts page

--- bundles/models.py
+++ bundles/models.py
@@ -6,8 +6,6 @@ class Bundle(models.Model):
     description = models.TextField()
     food_categories = models.ManyToManyField(Category, related_name='food_bundle', limit_choices_to={'type': 'food'}, blank=True)
     drink_categories = models.ManyToManyField(Category, related_name='drink_bundle', limit_choices_to={'type': 'drink'}, blank=True)
-    food_tokens = models.IntegerField(default=0, help_text="Total food tokens available for this bundle")
-    drink_tokens = models.IntegerField(default=0, help_text="Total drink tokens available for this bundle")
     number_of_people = models.IntegerField(default=1)
     playtime_hours = models.DecimalField(max_digits=4, decimal_places=2, default=0.0)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
