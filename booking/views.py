a00ba3c9c387ae2d14fb8c49d29e6ca406dd278d
Leigh Allen <lallenation@gmail.com>
2024-07-18 20:48:04 +0000

    feat: add booking confirmation view

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render, redirect
-from .forms import BookingForm
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .forms import BookingForm
+from .models import Booking
 from bundles.models import Bundle
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 @login_required
 def booking_view(request):
@@ -19,11 +24,13 @@ def booking_view(request):
             booking = form.save(commit=False)
             booking.user = request.user
             booking.save()
-            return redirect('myaccount:myaccount')
-        else:
-            pass 
-            
+            return redirect('booking:booking_confirmation', booking_id=booking.id)
     else:
         form = BookingForm(initial=initial_data)
 
     return render(request, 'booking.html', {'form': form})
+
+@login_required
+def booking_confirmation(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    return render(request, 'booking_confirmation.html', {'booking': booking})
