4d2281c91b47ef0d252c659a68cc984b4a6082c6
Leigh Allen <lallenation@gmail.com>
2024-07-23 16:11:37 +0000

    update: add edit booking url

--- myaccount/urls.py
+++ myaccount/urls.py
@@ -5,6 +5,5 @@ app_name = 'myaccount'
 
 urlpatterns = [
     path('', views.myaccount_view, name='myaccount'),
-     path('cancel/<int:booking_id>/', views.cancel_booking_view, name='cancel_booking'),
+    path('edit/<int:booking_id>/', views.edit_booking_view, name='edit_booking'),
 ]
-
